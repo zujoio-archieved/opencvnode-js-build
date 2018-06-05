deccca2e118ee8f4dff6cfb6e7e9f7b20aa8c9ba
arjun-kava <arjunkava91@gmail.com>
2018-06-05 09:29:19 +0530

    added publish to npm script

---
+++ scripts/publish.sh
@@ -0,0 +1,40 @@
+# script used to publish package to npm and push new tag
+# step 1 - change version from package json
+# step 2 - run ./scripts/publish-npm.sh $version  i.e ./scripts/publish-npm.sh 0.0.3
+set -e
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+ORIGIN=`git config --get remote.origin.url`
+
+if [ "$BRANCH" != "master" ]; then
+  echo "Error: Switch to the master branch before publishing."
+  exit
+fi
+
+if ! [[ "$ORIGIN" =~ opencvnode-js-build ]]; then
+  echo "Error: Switch to the main repo (opencvnode-js-build) before publishing."
+  exit
+fi
+
+# Build CPU:
+npm pack
+npm publish
+
+echo 'Published CPU-VERSION a new package to npm.'
+
+# Build GPU:
+sed -i -e 's/opencvnode-js-build"/opencvnode-js-build-gpu"/' package.json
+
+npm pack
+npm publish
+echo 'Published GPU-VERSION a new package to npm.'
+
+# Revert GPU changes:
+git checkout .
+
+if [ $# -ne 0 ]
+  then
+    git tag $1
+    git push --tags
+    rm -rf opencvnode-js-build-$1-tgz opencvnode-js-build-gpu-$1-tgz
+fi
